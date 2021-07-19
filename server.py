816bc90ff8f3565bd2cfa87ab161f6d8c36c1d1e
Michael Ortiz <ortiz.d.michael@gmail.com>
2021-07-18 23:02:36 -0700

    Adding initial server

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+import ssl
+
+def run_tls_server():
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain('/path/to/certchain.pem', '/path/to/private.key')
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
+        sock.bind(('127.0.0.1', 8443))
+        sock.listen(5)
+        with context.wrap_socket(sock, server_side=True) as ssock:
+            conn, addr = ssock.accept()
+    
+if __name__ == "__main__":
+    run_tls_server()
